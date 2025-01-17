ecc6757ca033607b54b94ff21b1a95904ec24223
jroy <jayyk0912@gmail.com>
2025-01-17 01:02:45 -0600

    figuring out ui libraries

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,22 @@ import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json'
 import path from 'path'
-
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   plugins: [react(), crx({ manifest })],
+  build: {
+    cssCodeSplit: false, // Bundle all CSS into a single file
+  },
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer(),
+      ],
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
